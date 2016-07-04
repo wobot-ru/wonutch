f987725de588f10597ce6adf2cc340ef3d70ae69
lmisakyan <l.misakyan@gmail.com>
2016-07-04 18:32:55 +0300

    Export script added.

---
+++ build-conf/bin/nutch2kafka.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export FLINK_HOME=/opt/flink-1.1-SNAPSHOT
+export FLINK_BIN=$FLINK_HOME/bin
+
+
+if [ -z "$1" ]; then
+	echo "Segment not defined."
+	echo "Usages: nutch2kafka <segment_path>"
+fi
+
+SEGMENT=hdfs://hdp-01/user/nutch/"$1"
+
+if [ -n "$1" ]; then
+        $FLINK_BIN/flink run --yarnname FB-To-Kafka --jobmanager yarn-cluster -yn 10 -ys 19 -yjm 4096 -ytm 4096 -yt $FLINK_HOME/lib $FLINK_HOME/etl/flink-nutch-0.2.0-SNAPSHOT.jar --nutch --nutch-extract --nutch-publish --bootstrap.servers "hdp-01:6667,hdp-02:6667,hdp-03:6667,hdp-04:6667" --seg $SEGMENT --topic-post FB-POST --topic-profile FB-PROFILE --batch-size 1 --request.timeout.ms 60000
+fi
+
+
