b2ea48c1b85a61a9872c061e9224e809f3295cc5
lmisakyan <l.misakyan@gmail.com>
2016-09-14 00:01:56 +0300

    Crawl scripts fixed.

--- build-conf/bin/nutch-2-kafka.sh
+++ build-conf/bin/nutch-2-kafka.sh
@@ -13,7 +13,7 @@ fi
 SEGMENT=hdfs://hdp-01/user/nutch/"$1"
 
 if [ -n "$1" ]; then
-   $FLINK_BIN/flink run --yarnname FB-To-Kafka --jobmanager yarn-cluster -yn 4 -ys 8 -yjm 5120 -ytm 8096 -yt $FLINK_HOME/lib $FLINK_HOME/lib/etl-0.2.0-SNAPSHOT.jar --nutch --nutch-extract --nutch-publish --bootstrap.servers "hdp-01:6667,hdp-02:6667,hdp-03:6667,hdp-04:6667" --seg $SEGMENT --topic-post FB-POST --topic-profile FB-PROFILE --batch-size 1 --request.timeout.ms 60000
+   $FLINK_BIN/flink run --yarnname FB-To-Kafka --jobmanager yarn-cluster -yn 4 -ys 8 -yjm 5120 -ytm 8096 -yt $FLINK_HOME/lib $FLINK_HOME/lib/etl-0.2.0-SNAPSHOT.jar --nutch --nutch-extract --nutch-publish --bootstrap.servers "hdp-01:6667,hdp-02:6667,hdp-03:6667,hdp-04:6667" --seg $SEGMENT --topic-post FB-POST --topic-detailed-post FB-MERGED-POST --topic-profile FB-PROFILE --batch-size 1 --request.timeout.ms 60000
 fi
 
 
